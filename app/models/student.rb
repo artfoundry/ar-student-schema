894168aeeda10cf73c73c0489c98f1d688946948
guillaumegaluz <guillaume.galuz@gmail.com>
2013-09-12 21:06:51 -0700

    final submission

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, :format => {:with => /.+@.+\...+/, 
+    :message => "email must be in the format \"something@domain.com\""}
+
+  validates :email, :uniqueness => true
+  
+  validates :phone, :format => {:with => /\(\d{3}\)\W\d{3}\W\d{4}.*/,
+    :message => "phone must be in the format \"XXX-XXX-XXXX\""}
+
+  validates_numericality_of :age, :greater_than => 5
+
+  def name
+    full_name = first_name + " " + last_name
+  end
+
+  def age
+    age = Time.now.year - birthday.year
+  end
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,15 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
-
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table(:students) do |t|
+      t.column :first_name, :string
+      t.column :last_name, :string
+      t.column :gender, :string
+      t.column :birthday, :date
+      t.column :email, :string
+      t.column :phone, :string
+      t.timestamps
+    end
   end
 end
